6ef1d91dacc5a8c632f75bad083cc029cf0b880a
TheCreeperCow <lars.dormans@live.nl>
2019-05-07 09:38:34 +0200

    Changed Provider to not reveal secret keys (Keys will be given thru a secret snippet)

--- backend/Provider.php
+++ backend/Provider.php
@@ -11,14 +11,22 @@ use League\OAuth2\Client\Provider\GenericProvider;
  */
 class FrontierAuth
 {
-    private const CLIENT_ID = '4e0ab7ed-0eba-44ba-b494-4a99435367b8';
-
-    private const CLIENT_SECRET = '2dd74d80-d9e6-4625-ab64-20a38a434c63';
-
+    private $client_id = null;
+    private $client_secret = null;
     private const AUTH_API = 'https://auth.frontierstore.net';
 
     //changed url to work in my directory system, adjust as necessary for actual site - HW
-	private const CALLBACK_URL = 'http://development.localhost/journal-reader/backend/Callback.php'; //TODO Change to the callback of the actual application
+	private const CALLBACK_URL = 'http://development.localhost/journal-reader/backend/Callback.php';//TODO Change to the callback of the actual application
+
+    /**
+     * FrontierAuth constructor.
+     */
+    public function __construct()
+    {
+        $json = json_decode(file_get_contents(__DIR__."/../keys.json"));
+        $this->client_id = $json->Client;
+        $this->client_secret = $json->Secret;
+    }
 
     /**
      * @return GenericProvider
@@ -27,8 +35,8 @@ class FrontierAuth
     {
         return new GenericProvider([
             'scope' => 'auth capi',
-            'clientId' => self::CLIENT_ID,
-            'clientSecret' => self::CLIENT_SECRET,
+            'clientId' => $this->client_id,
+            'clientSecret' => $this->client_secret,
             'redirectUri' => self::CALLBACK_URL,
             'urlAuthorize' => self::AUTH_API . '/auth',
             'urlAccessToken' => self::AUTH_API . '/token',
