8bf27d9a7f49945ea660eb007c4687a953956446
Hack-Wizard <hack-wizard@is-a-furry.org>
2019-05-11 15:08:14 -0700

    Fixed page code so script is properly embedded instead of ran to complete html

--- result.php
+++ result.php
@@ -28,36 +28,101 @@
 	</style>
 </head>
 <body>
-<h1>Journal Reader Results</h1><br>
-<h2>Copy and paste the following into the chat as requested by dispatch</h2><br>
-<?php
-/**
- * Created by PhpStorm.
- * User: Lars
- * Date: 30/03/2019
- * Time: 14:48
- */
-session_start();
-$info = $_SESSION['info'];
-// added echo formatting - HW
-echo "<div align=\x22center\x22>";
-echo "<h3>Ship info</h3>";
-echo "<p>Current Location: ".$info['system']."</p>";
-echo "<p>Current hull percentage: ".$info['hull']."</p>";
-// adding color to these lines and logic so it doesn't show o2 or ls for no breach - HW
-echo "<p class=\x22critical1\x22>Is canopy breached: ".$info['breach']."</p>";
-if ( $info['breach'] == 'true' ) {
-	echo "<p class=\x22critical2\x22>Oxygen in minutes: ".$info['oxygen']."</p>";
-	echo "<p>Life support synth count: ".$info['lifesupport']."</p>";
-}
+	<div id="home">
+        <header>
+            <nav class="navbar container navbar-expand-lg navbar-expand-md navbar-dark" role="navigation">
+                <a class="navbar-brand" href="../index.html">
+                    <img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo" /> Hull Seals</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../knowledge">Knowledge Base</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Chat</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://forums.frontier.co.uk/showthread.php/452349-DW2-Fleet-Mechanics">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://inara.cz/squadron/5050/">Inara Squadron</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+		<section class="introduction">
+            <article>
+				<h1>Journal Reader Results</h1><br>
+				<h2>Copy and paste the following into the chat as requested by dispatch</h2><br>
+				<div align="center">
+					<h3>Ship info</h3>
+					<?php
+						/**
+						* Created by PhpStorm.
+						* User: Lars
+						* Date: 30/03/2019
+						* Time: 14:48
+						*/
+						session_start();
+						$info = $_SESSION['info'];
+						// added echo formatting - HW
+						echo "<p>Current Location: ".$info['system']."</p>";
+						echo "<p>Current hull percentage: ".$info['hull']."</p>";
+						// adding color to these lines and logic so it doesn't
+						// show o2 or ls for no breach - HW
+						echo "<p class=\x22critical1\x22>Is canopy breached: ".$info['breach']."</p>";
+						if ( $info['breach'] == 'true' ) {
+							echo "<p class=\x22critical2\x22>Oxygen in minutes: ".$info['oxygen']."</p>";
+							echo "<p>Life support synth count: ".$info['lifesupport']."</p>";
+						}
 
-/** TODO - add lines for SRV Data - HW
- *  echo "<h3>SRV Coordinates</h3>";
- *  
- */
- 
-echo "</div>";
-// Commenting out the debug dump - HW
-// var_dump($_SESSION['debug']);
+						/** TODO - add lines for SRV Data - HW
+						*  echo "<h3>SRV Coordinates</h3>";
+						*  
+						*/
+						
+						// Commenting out the debug dump - HW
+						// var_dump($_SESSION['debug']);
+					?>					
+				</div>
+			</article>
+			<div class="clearfix"></div>
+		</section>
+	</div>
+	<footer class="page-footer font-small">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9 mt-md-0 mt-3">
+                    <h5 class="text-uppercase">Hull Seals</h5>
+                    <p>
+                        <em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
+                    <a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
+                </div>
+                <hr class="clearfix w-100 d-md-none pb-3" />
+                <div class="col-md-3 mb-md-0 mb-3">
+                    <h5 class="text-uppercase">Links</h5>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="https://twitter.com/HullSeals">
+                                <img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20" /> Twitter</a>
+                        </li>
+                        <li>
+                            <a href="../donate">Donate</a>
+                        </li>
+                        <li>
+                            <a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy &amp; Cookies
+							Policy</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.</div>
+    </footer>
+</body>
 
-echo "</body>";
+</html>
