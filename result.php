e40bd5e9b641296704a36941313f9e48152874d5
Rixxan <rixxan@hullseals.space>
2019-05-18 14:56:57 -0400

    Update result.php

--- result.php
+++ result.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html  lang="en">
 <head>
-	<link href="favicon.ico" rel="icon" type="image/x-icon">
-	<link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+	<link href="../favicon.ico" rel="icon" type="image/x-icon">
+	<link href="../favicon.ico" rel="shortcut icon" type="image/x-icon">
 	<meta charset="UTF-8">
 	<meta content="Wolfii Namakura" name="author">
 	<meta content="hull seals, elite dangerous, distant worlds, seal team fix, mechanics, dw2" name="keywords">
@@ -29,31 +29,38 @@
 </head>
 <body>
 	<div id="home">
-        <header>
-            <nav class="navbar container navbar-expand-lg navbar-expand-md navbar-dark" role="navigation">
-                <a class="navbar-brand" href="../index.html">
-                    <img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo" /> Hull Seals</a>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="../index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../knowledge">Knowledge Base</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chat</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://forums.frontier.co.uk/showthread.php/452349-DW2-Fleet-Mechanics">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://inara.cz/squadron/5050/">Inara Squadron</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+		<header>
+				<nav class="navbar container navbar-expand-lg navbar-expand-md navbar-dark" role="navigation">
+						<a class="navbar-brand" href="#"><img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo"> Hull Seals</a>
+
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+								<span class="navbar-toggler-icon"></span>
+						</button>
+
+						<div class="collapse navbar-collapse" id="navbarNav">
+								<ul class="navbar-nav">
+										<li class="nav-item">
+												<a class="nav-link" href="../index.html">Home</a>
+										</li>
+										<li class="nav-item">
+												<a class="nav-link" href="../knowledge">Knowledge Base</a>
+										</li>
+										<li class="nav-item active">
+												<a class="nav-link" href="../journal">Journal Reader</a>
+										</li>
+										<li class="nav-item">
+												<a class="nav-link" href="../about">About</a>
+										</li>
+										<li class="nav-item">
+												<a class="nav-link" href="../contact">Contact</a>
+										</li>
+										<li class="nav-item">
+												<a class="nav-link" href="#">Login/Register</a>
+										</li>
+								</ul>
+						</div>
+				</nav>
+		</header>
 		<section class="introduction">
             <article>
 				<h1>Journal Reader Results</h1><br>
@@ -83,9 +90,9 @@
 
 						/** TODO - add lines for SRV Data - HW
 						*  echo "<h3>SRV Coordinates</h3>";
-						*  
+						*
 						*/
-						
+
 						// Commenting out the debug dump - HW
 						// var_dump($_SESSION['debug']);
 					?>
@@ -114,35 +121,29 @@
 		</section>
 	</div>
 	<footer class="page-footer font-small">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-9 mt-md-0 mt-3">
-                    <h5 class="text-uppercase">Hull Seals</h5>
-                    <p>
-                        <em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
-                    <a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
-                </div>
-                <hr class="clearfix w-100 d-md-none pb-3" />
-                <div class="col-md-3 mb-md-0 mb-3">
-                    <h5 class="text-uppercase">Links</h5>
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="https://twitter.com/HullSeals">
-                                <img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20" /> Twitter</a>
-                        </li>
-                        <li>
-                            <a href="../donate">Donate</a>
-                        </li>
-                        <li>
-                            <a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy &amp; Cookies
-							Policy</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.</div>
-    </footer>
+			<div class="container">
+					<div class="row">
+							<div class="col-md-9 mt-md-0 mt-3">
+									<h5 class="text-uppercase">Hull Seals</h5>
+									<p><em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
+				<a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
+							</div>
+							<hr class="clearfix w-100 d-md-none pb-3">
+							<div class="col-md-3 mb-md-0 mb-3">
+									<h5 class="text-uppercase">Links</h5>
+
+									<ul class="list-unstyled">
+											<li><a href="https://twitter.com/HullSeals" target="_blank"><img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20"></a> <a href="https://reddit.com/r/HullSeals" target="_blank"><img alt="Reddit" height="20" src="../images/reddit.png" width="20"></a> <a href="https://www.youtube.com/channel/UCwKysCkGU_C6V8F2inD8wGQ" target="_blank"><img alt="Youtube" height="20" src="../images/youtube.png" width="20"></a> <a href="https://www.twitch.tv/hullseals" target="_blank"><img alt="Twitch" height="20" src="../images/twitch.png" width="20"></a> <a href="https://gitlab.com/hull-seals-cyberseals" target="_blank"><img alt="GitLab" height="20" src="../images/gitlab.png" width="20"></a></li>
+					<li><a href="/donate">Donate</a></li>
+											<li><a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy & Cookies Policy</a></li>
+									</ul>
+							</div>
+					</div>
+			</div>
+			<div class="footer-copyright">
+					Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.
+			</div>
+	</footer>
 </body>
 
 </html>
