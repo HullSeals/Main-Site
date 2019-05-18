01b3904e99f7b58cd89944adf5fa0e8a2d4a3b4f
Rixxan <rixxan@hullseals.space>
2019-05-18 14:37:17 -0400

    Layout Update

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
     <meta content="Welcome to the Hull Seals, Elite Dangerous's Premier Hull Repair Specialists!" name="description">
     <title>Journal Reader | The Hull Seals</title>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -50,24 +52,31 @@
     <div id="home">
         <header>
             <nav class="navbar container navbar-expand-lg navbar-expand-md navbar-dark" role="navigation">
-                <a class="navbar-brand" href="../index.html">
-                    <img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo" /> Hull Seals</a>
+                <a class="navbar-brand" href="#"><img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo"> Hull Seals</a>
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        <li class="nav-item active">
+                        <li class="nav-item">
                             <a class="nav-link" href="../index.html">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="../knowledge">Knowledge Base</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chat</a>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../journal">Journal Reader</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://forums.frontier.co.uk/showthread.php/452349-DW2-Fleet-Mechanics">About</a>
+                            <a class="nav-link" href="../about">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://inara.cz/squadron/5050/">Inara Squadron</a>
+                            <a class="nav-link" href="../contact">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login/Register</a>
                         </li>
                     </ul>
                 </div>
@@ -86,30 +95,24 @@
             <div class="row">
                 <div class="col-md-9 mt-md-0 mt-3">
                     <h5 class="text-uppercase">Hull Seals</h5>
-                    <p>
-                        <em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
-                    <a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
+                    <p><em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
+					<a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
                 </div>
-                <hr class="clearfix w-100 d-md-none pb-3" />
+                <hr class="clearfix w-100 d-md-none pb-3">
                 <div class="col-md-3 mb-md-0 mb-3">
                     <h5 class="text-uppercase">Links</h5>
+
                     <ul class="list-unstyled">
-                        <li>
-                            <a href="https://twitter.com/HullSeals">
-                                <img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20" /> Twitter</a>
-                        </li>
-                        <li>
-                            <a href="../donate">Donate</a>
-                        </li>
-                        <li>
-                            <a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy &amp; Cookies
-							Policy</a>
-                        </li>
+                        <li><a href="https://twitter.com/HullSeals" target="_blank"><img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20"></a> <a href="https://reddit.com/r/HullSeals" target="_blank"><img alt="Reddit" height="20" src="../images/reddit.png" width="20"></a> <a href="https://www.youtube.com/channel/UCwKysCkGU_C6V8F2inD8wGQ" target="_blank"><img alt="Youtube" height="20" src="../images/youtube.png" width="20"></a> <a href="https://www.twitch.tv/hullseals" target="_blank"><img alt="Twitch" height="20" src="../images/twitch.png" width="20"></a> <a href="https://gitlab.com/hull-seals-cyberseals" target="_blank"><img alt="GitLab" height="20" src="../images/gitlab.png" width="20"></a></li>
+						<li><a href="/donate">Donate</a></li>
+                        <li><a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy & Cookies Policy</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-        <div class="footer-copyright">Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.</div>
+        <div class="footer-copyright">
+            Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.
+        </div>
     </footer>
 </body>
 
