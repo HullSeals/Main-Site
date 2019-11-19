99116171124be27feecf0d7c1ee002e2dad793ad
David Sangrey <davidsangrey@gmail.com>
2019-11-19 04:43:24 +0000

    Merge branch 'beta' into 'master'
    Beta
    
    See merge request hull-seals-cyberseals/code/on-ice/hull-seals-main-site!2
    
    (cherry picked from commit 607e2424871d5a71252daa15fcac9e3f04a48d7a)
    
    9ccc11df Repair Form Beta Test
    a131b013 Merge branch 'Repair-Request-Form' into 'beta'
    5e718b55 Begin Paperwork Form
    ac66e74d Update New Form
    97ada638 Update index.php
    0a574c16 Remove IP tracking, we can pass data now
    72635ab5 Remove unused code.
    64677de4 Add CloudFlare data & Fixes
    03769d56 Add IP Check to Repair Form
    73823e81 Create Success page
    1c41350a Update All
    37efe7a9 Remove Unused Images

--- paperwork/fishers/success.php
+++ paperwork/fishers/success.php
@@ -1,3 +1,65 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$ip='Unable To Log';
+$cloudflareIPRanges = array(
+    '204.93.240.0/24',
+    '204.93.177.0/24',
+    '199.27.128.0/21',
+    '173.245.48.0/20',
+    '103.21.244.0/22',
+    '103.22.200.0/22',
+    '103.31.4.0/22',
+    '141.101.64.0/18',
+    '108.162.192.0/18',
+    '190.93.240.0/20',
+    '188.114.96.0/20',
+    '197.234.240.0/22',
+    '198.41.128.0/17',
+    '162.158.0.0/15'
+);
+ 
+//NA by default.
+$ip = 'NA';
+ 
+//Check to see if the CF-Connecting-IP header exists.
+if(isset($_SERVER["HTTP_CF_CONNECTING_IP"])){
+    
+    //Assume that the request is invalid unless proven otherwise.
+    $validCFRequest = false;
+    
+    //Make sure that the request came via Cloudflare.
+    foreach($cloudflareIPRanges as $range){
+        //Use the ip_in_range function from Joomla.
+        if(ip_in_range($_SERVER['REMOTE_ADDR'], $range)) {
+            //IP is valid. Belongs to Cloudflare.
+            $validCFRequest = true;
+            break;
+        }
+    }
+    
+    //If it's a valid Cloudflare request
+    if($validCFRequest){
+        //Use the CF-Connecting-IP header.
+        $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    } else{
+        //If it isn't valid, then use REMOTE_ADDR. 
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    
+} else{
+    //Otherwise, use REMOTE_ADDR.
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+ 
+//Define it as a constant so that we can
+//reference it throughout the app.
+define('IP_ADDRESS', $ip);
+
+//$lgd_ip='notLogged';
+$lgd_ip=$ip;
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -8,7 +70,7 @@
         <meta content="hull seals, elite dangerous, distant worlds, seal team fix, mechanics, dw2" name="keywords">
         <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0" name="viewport">
         <meta content="Welcome to the Hull Seals, Elite Dangerous's Premier Hull Repair Specialists!" name="description">
-        <title>Repair Requests | The Hull Seals</title>
+        <title>Kingfisher Paperwork | The Hull Seals</title>
         <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -42,9 +104,17 @@
                     "content": {
                         "href": "https://hullseals.space/knowledge/books/important-information/page/cookie-policy"
                     }
-                })
+                });
             });
         </script>
+        <style>
+            .input-group-prepend input[type="checkbox"] {
+                margin-right: 5px;
+            }
+            label {
+                user-select: none;
+            }
+        </style>
     </head>
 
     <body>
@@ -83,28 +153,9 @@
             </header>
             <section class="introduction">
                 <article>
-                    <h1>Request Repairs</h1>
-                    <p>
-                        <strong>
-                            Do you see a countdown timer?
-                        </strong>
-                        <br />
-                        <em>
-                            If so, log out to the menu immediately
-                        </em>
-                        <br />
-                        <br />
-                        Do you need repairs or Code Black canopy breach services?
-                        <br />
-                        <a href="case.php" class="btn btn-success btn-lg">Yes, I Need Repairs</a>
-                        <br />
-                        <br />
-                        Just want to talk or join up?
-                        <br />
-                        <a href="#" class="btn btn-secondary btn-lg">Just Chatting!</a>                  
-                    </p>
+                    <h1>Kingfisher Case Paperwork</h1>
+					<h5 class="text-success">Thank you for submitting your paperwork, Fisher! You may now close the tab.</h5>
                 </article>
-                <div class="clearfix"></div>
             </section>
         </div>
         <footer class="page-footer font-small">
@@ -128,8 +179,9 @@
                 </div>
             </div>
             <div class="footer-copyright">
-                Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.
+                Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc. <span class="float-right pr-3" title="Your IP might be logged for security reasons"><img src="ip-icon.png" witdh="16" height="16" alt="IP"/> Logged - <?php echo $ip ?></span>
             </div>
         </footer>
     </body>
+
 </html>
