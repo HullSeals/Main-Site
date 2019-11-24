9354dd4372e833b60194b7abc02b48ec2ebd1fdd
Rixxan <rixxan@hullseals.space>
2019-11-24 14:40:54 -0500

    Move About, Contact, Main to PHP

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
                             <a class="nav-link" href="contact">Contact</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://auth.hullseals.space/">Login/Register</a>
+                            <a class="nav-link" href="https://dev.hullseals.space/users/">Login/Register</a>
                         </li>
                     </ul>
                 </div>
