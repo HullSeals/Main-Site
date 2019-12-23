64fe63716756acbc793009a004136a1e9b15c505
Rixxan <rixxan@hullseals.space>
2019-12-22 19:49:01 -0500

    Update head_tags.php

--- usersc/includes/head_tags.php
+++ usersc/includes/head_tags.php
@@ -3,10 +3,10 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta name="description" content="">
+<meta name="description" content="Welcome to the Hull Seals, Elite Dangerous's Premier Hull Repair Specialists!">
 <meta name="author" content="">
 <meta property="og:url"                content="" /> <!-- URL for website (link address) -->
 <meta property="og:type"               content="website" /> <!-- type of site -->
-<meta property="og:title"              content="Userspice Site" /> <!-- title of site (title of share) -->
-<meta property="og:description"        content="Powered by UserSpice" /> <!-- description of site (text which appears when sharing) -->
-<meta property="og:image"              content="" /> <!-- URL for preview image -->
+<meta property="og:title"              content="The Hull Seals" /> <!-- title of site (title of share) -->
+<meta property="og:description"        content="Password Restricted Link" /> <!-- description of site (text which appears when sharing) -->
+<meta property="og:image"              content="https://hullseals.space/images/emblem_loss.png" /> <!-- URL for preview image -->
