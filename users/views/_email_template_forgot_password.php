549b2b0e4d8228df7d43c77db7760bd71919342a
Rixxan <rixxan@hullseals.space>
2019-12-25 23:00:16 -0500

    Update Email Template, SVG

--- users/views/_email_template_forgot_password.php
+++ users/views/_email_template_forgot_password.php
@@ -9,7 +9,7 @@ $results = $query->first();
   <meta charset="utf-8">
 </head>
 <body>
-  <p><?=lang("EML_HI")?> <?=$username;?>,</p>
+  <p><?=lang("EML_HI")?> <?=$fname;?>,</p>
   <p><?=lang("EML_WHY")?></p>
   <p><?=lang("EML_HOW")?></p>
   <p><a href="<?php echo $results->verify_url."users/forgot_password_reset.php?email=".$email."&vericode=$vericode&reset=1"; ?>" class="nounderline"><?=lang("PW_RESET");?></a></p>
