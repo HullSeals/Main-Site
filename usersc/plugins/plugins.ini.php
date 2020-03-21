75b6d3d5907351acba04fd8ed27bdb232a66b92b
Rixxan <rixxan@hullseals.space>
2020-03-21 16:10:17 -0400

    Active US Plugins & New Hook

--- usersc/plugins/plugins.ini.php
+++ usersc/plugins/plugins.ini.php
@@ -1,5 +1,10 @@
 ;<?php die();?>
 hooker = 1
 session_manager = 1
-bio = 1
-profile_pic = 2
+bio = 2
+profile_pic = 1
+messages = 1
+notifications = 1
+forms = 1
+community_functions = 1
+defaultrememberme = 1
