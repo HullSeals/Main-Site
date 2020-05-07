c8326d6af32242e09628351a97afad021f1e4d4f
Rixxan <rixxan@hullseals.space>
2020-05-07 19:09:32 -0400

    Fix "First CMDR" bug

--- cmdr_management/index.php
+++ cmdr_management/index.php
@@ -103,7 +103,36 @@ if (isset($_SESSION['2ndrun'])) {
     $stmt->bind_param("i", $user->data()->id);
     $stmt->execute();
     $result = $stmt->get_result();
-    if($result->num_rows === 0) exit('No CMDRs');
+    if($result->num_rows === 0) exit('<a href="new_alias.php" class="btn btn-success btn-lg active" >Register a New CMDR</a>
+    </article>
+    <div class="clearfix"></div>
+    </section>
+    </div>
+    <footer class="page-footer font-small">
+    <div class="container">
+    <div class="row">
+    <div class="col-md-9 mt-md-0 mt-3">
+    <h5 class="text-uppercase">Hull Seals</h5>
+    <p><em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
+    <a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
+    </div>
+    <hr class="clearfix w-100 d-md-none pb-3">
+    <div class="col-md-3 mb-md-0 mb-3">
+    <h5 class="text-uppercase">Links</h5>
+    <ul class="list-unstyled">
+    <li><a href="https://twitter.com/HullSeals" target="_blank"><img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20"></a> <a href="https://reddit.com/r/HullSeals" target="_blank"><img alt="Reddit" height="20" src="../images/reddit.png" width="20"></a> <a href="https://www.youtube.com/channel/UCwKysCkGU_C6V8F2inD8wGQ" target="_blank"><img alt="Youtube" height="20" src="../images/youtube.png" width="20"></a> <a href="https://www.twitch.tv/hullseals" target="_blank"><img alt="Twitch" height="20" src="../images/twitch.png" width="20"></a> <a href="https://gitlab.com/hull-seals-cyberseals" target="_blank"><img alt="GitLab" height="20" src="../images/gitlab.png" width="20"></a></li>
+    <li><a href="/donate">Donate</a></li>
+    <li><a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy & Cookies Policy</a></li>
+    </ul>
+    </div>
+    </div>
+    </div>
+    <div class="footer-copyright">
+    Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.
+    </div>
+    </footer></body>
+    </html>
+');
     echo "<h3>Returning all Registered CMDR Aliases for: ";
     echo echousername($user->data()->id);
     echo nl2br ("</h3>");
--- cmdr_management/new_alias.php
+++ cmdr_management/new_alias.php
@@ -174,7 +174,35 @@ if (isset($_GET['send'])) {
                         <button type="submit" class="btn btn-primary">Submit</button> <a href="." class="btn btn-warning">Go Back</a>
                     </form>
                     <?php
-    if($numAlias->num_rows === 0) exit('This will be your First Alias');
+    if($numAlias->num_rows === 0) exit('This will be your First Alias  </article>
+      <div class="clearfix"></div>
+      </section>
+      </div>
+      <footer class="page-footer font-small">
+      <div class="container">
+      <div class="row">
+      <div class="col-md-9 mt-md-0 mt-3">
+      <h5 class="text-uppercase">Hull Seals</h5>
+      <p><em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
+      <a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
+      </div>
+      <hr class="clearfix w-100 d-md-none pb-3">
+      <div class="col-md-3 mb-md-0 mb-3">
+      <h5 class="text-uppercase">Links</h5>
+      <ul class="list-unstyled">
+      <li><a href="https://twitter.com/HullSeals" target="_blank"><img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20"></a> <a href="https://reddit.com/r/HullSeals" target="_blank"><img alt="Reddit" height="20" src="../images/reddit.png" width="20"></a> <a href="https://www.youtube.com/channel/UCwKysCkGU_C6V8F2inD8wGQ" target="_blank"><img alt="Youtube" height="20" src="../images/youtube.png" width="20"></a> <a href="https://www.twitch.tv/hullseals" target="_blank"><img alt="Twitch" height="20" src="../images/twitch.png" width="20"></a> <a href="https://gitlab.com/hull-seals-cyberseals" target="_blank"><img alt="GitLab" height="20" src="../images/gitlab.png" width="20"></a></li>
+      <li><a href="/donate">Donate</a></li>
+      <li><a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy & Cookies Policy</a></li>
+      </ul>
+      </div>
+      </div>
+      </div>
+      <div class="footer-copyright">
+      Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.
+      </div>
+      </footer></body>
+      </html>
+');
     echo "<br />";
     echo "<h4>This is alias # ";
     echo $numAlias->num_rows+1;
