b252ed1488acee7eb9de2815cfb1635c46e74b8c
Rixxan <rixxan@hullseals.space>
2020-07-09 15:24:27 -0400

    Add css... oops

--- seal-links/index.php
+++ seal-links/index.php
@@ -18,7 +18,7 @@ $logged_in = $user->data();
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
     <script src="https://hullseals.space/assets/javascript/allPages.js" integrity="sha384-PsQdnKGi+BdHoxLI6v+pi6WowfGtnraU6GlDD4Uh5Qw2ZFiDD4eWNTNG9+bHL3kf" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    
+    <link rel="stylesheet" type="text/css" href="https://hullseals.space/assets/css/allPages.css" />
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
     <style>
     .links article {
