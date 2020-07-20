e23beb077e0b54c9507c7334cfbe111c147f1199
Rixxan <rixxan@hullseals.space>
2020-07-20 13:11:34 -0400

    Create week3.php

---
+++ TLA/week3.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="../favicon.ico" rel="icon" type="image/x-icon">
+<link href="../favicon.ico" rel="shortcut icon" type="image/x-icon">
+<meta charset="UTF-8">
+<meta content="Wolfii Namakura" name="author">
+<meta content="hull seals, elite dangerous, distant worlds, seal team fix, mechanics, dw2" name="keywords">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0" name="viewport">
+<meta content="Events for The Long Arf" name="description">
+<title>TLA - Week 3 | The Hull Seals</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<script src="https://hullseals.space/assets/javascript/allPages.js" integrity="sha384-PsQdnKGi+BdHoxLI6v+pi6WowfGtnraU6GlDD4Uh5Qw2ZFiDD4eWNTNG9+bHL3kf" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
+<link href="https://hullseals.space/assets/css/Centercss.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  <div id="home">
+  <header>
+    <nav class="navbar container navbar-expand-lg navbar-expand-md navbar-dark" role="navigation">
+    <a class="navbar-brand" href="../"><img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo"> Hull Seals</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+    <li class="nav-item">
+    <a class="nav-link" href="index.php">TLA Expedition Home</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="faq.php">TLA FAQ</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="crew.php">Carrier Crew</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="about.php">About TLA</a>
+    </li>
+    <li class="nav-item active">
+    <a class="nav-link" href="route.php">Route</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="resources.php">TLA Resources</a>
+    </li>
+    </ul>
+    </div>
+    </nav>
+  </header>
+  <section class="introduction container">
+  <article>
+    <h1>Week 3</h1>
+    <br>
+    <p><b>Week 3: Halfway Point to The Drop Zone</b><p>
+      <p>The penultimate leg: The Limpet's Call explores the oddities of the galaxy, as order is restored.</p>
+      <img src="images/maps/3WBW.png" alt="The Third Week's Route" width="300" style="float: right;"/>
+      <p>The Waypoints:</p>
+      <ol>
+        <li>Halfway Point</li>
+        <li>Dewdrop Nebula</li>
+        <li>The Drop Zone</li>
+      </ol>
+      <p>You should arrive at the Drop Zone on the 25th of July.</p>
+      <p>Basecamps, if present, will be announced in Discord.</p>
+      <br>
+      <p>The Route:</p>
+      <table class="table table-dark table-striped table-bordered table-hover table-responsive-md">
+        <thead>
+        <tr>
+          <th>System</td>
+          <th>&Delta; Start</th>
+          <th>&Delta; Last</th>
+          <th>&Delta; End</th>
+          <th>Carrier Location</th>
+          <th>EDSM Link</th>
+          <th>Arrival Date</th>
+        </tr>
+      </thead>
+      <tr>
+        <td>Juenoi WB-J c23-256</td>
+        <td>8,525 LY</td>
+        <td>488 LY</td>
+        <td>8,413 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51037906/name/Juenoi+WB-J+c23-256" class="btn btn-secondary active">EDSM</a></td>
+        <td>19 July</td>
+      </tr>
+      <tr>
+        <td>Systimbau RL-X c1-196</td>
+        <td>9,007 LY</td>
+        <td>483 LY</td>
+        <td>7,938 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51039768/name/Systimbau+RL-X+c1-196" class="btn btn-secondary active">EDSM</a></td>
+        <td>21 July</td>
+      </tr>
+      <tr>
+        <td>Systimbau LA-G c11-349</td>
+        <td>9,489 LY</td>
+        <td>487 LY</td>
+        <td>7,462 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51156898/name/Systimbau+LA-G+c11-349" class="btn btn-secondary active">EDSM</a></td>
+        <td>21 July</td>
+      </tr>
+      <tr>
+        <td>Systimbo UG-D b41-10</td>
+        <td>9,976 LY</td>
+        <td>488 LY</td>
+        <td>6,979 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51157483/name/Systimbo+UG-D+b41-10" class="btn btn-secondary active">EDSM</a></td>
+        <td>21 July</td>
+      </tr>
+      <tr>
+        <td>Systimbo ZA-O e6-1528 (The Dewdrop Nebula)</td>
+        <td>10,287 LY</td>
+        <td>313 LY</td>
+        <td>6,674 LY</td>
+        <td>Systimbo ZD-E c26-62</td>
+        <td><a href="https://www.edsm.net/en/system/id/13313583/name/Systimbo+ZA-O+e6-1528" class="btn btn-secondary active">EDSM</a></td>
+        <td>22 July</td>
+      </tr>
+      <tr>
+        <td>Scaulao YE-P c6-447</td>
+        <td>10,773 LY</td>
+        <td>490 LY</td>
+        <td>6,184 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51162393/name/Scaulao+YE-P+c6-447" class="btn btn-secondary active">EDSM</a></td>
+        <td>24 July</td>
+      </tr>
+      <tr>
+        <td>Scaulao WU-V c16-238</td>
+        <td>11,254 LY</td>
+        <td>486 LY</td>
+        <td>5,698 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51163263/name/Scaulao+WU-V+c16-238" class="btn btn-secondary active">EDSM</a></td>
+        <td>24 July</td>
+      </tr>
+      <tr>
+        <td>Scaulao PE-E c26-228</td>
+        <td>11,730 LY</td>
+        <td>482 LY</td>
+        <td>5,215 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51164233/name/Scaulao+PE-E+c26-228" class="btn btn-secondary active">EDSM</a></td>
+        <td>25 July</td>
+      </tr>
+      <tr>
+        <td>Umbairrs QQ-N c7-457</td>
+        <td>12,215 LY</td>
+        <td>492 LY</td>
+        <td>4,724 LY</td>
+        <td></td>
+        <td><a href="https://www.edsm.net/en/system/id/51192645/name/Umbairrs+QQ-N+c7-457" class="btn btn-secondary active">EDSM</a></td>
+        <td>25 July</td>
+      </tr>
+      </table>
+      <h3>Next Week's Route will be released on the 26th of July.</h3>
+      <br>
+      <p>Your ship's navigational computer has been updated with the route.<br> Check it on <a href="https://www.edsm.net/en/expeditions/summary/id/99/name/The+Long+Arf" class="btn btn-lg btn-primary active">EDSM</a>
+  </article>
+  <div class="clearfix"></div>
+  </section>
+  </div>
+  <footer class="page-footer font-small">
+  <div class="container">
+  <div class="row">
+  <div class="col-md-9 mt-md-0 mt-3">
+  <h5 class="text-uppercase">Hull Seals</h5>
+  <p><em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
+  <a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
+  </div>
+  <hr class="clearfix w-100 d-md-none pb-3">
+  <div class="col-md-3 mb-md-0 mb-3">
+  <h5 class="text-uppercase">Links</h5>
+  <ul class="list-unstyled">
+  <li><a href="https://twitter.com/HullSeals" target="_blank"><img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20"></a> <a href="https://reddit.com/r/HullSeals" target="_blank"><img alt="Reddit" height="20" src="../images/reddit.png" width="20"></a> <a href="https://www.youtube.com/channel/UCwKysCkGU_C6V8F2inD8wGQ" target="_blank"><img alt="Youtube" height="20" src="../images/youtube.png" width="20"></a> <a href="https://www.twitch.tv/hullseals" target="_blank"><img alt="Twitch" height="20" src="../images/twitch.png" width="20"></a> <a href="https://gitlab.com/hull-seals-cyberseals" target="_blank"><img alt="GitLab" height="20" src="../images/gitlab.png" width="20"></a></li>
+  <li><a href="/donate">Donate</a></li>
+  <li><a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy & Cookies Policy</a></li>
+  </ul>
+  </div>
+  </div>
+  </div>
+  <div class="footer-copyright">
+  Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.
+  </div>
+  <!--[if IE 6]
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@&.&&&&&&&&&&&&&&&&&&&.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@.@&&&&&&&&&&&&&&&&&&&&&&.@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@.&&&&&&.............&&&&&&.@@@@@@@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@@@@*.&&&&&..             ./&&&&&.@@@@@@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@@@.@&(&(&.                 .&&&&&@.@@@@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@@@.&&.&,@,                   .&&&.&&.@@@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@@.*&&&&&.          &&&         ..&&&%&.*@@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@@.&&.&,&.         .,&&&.          .&..@&@.@@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@.&&&&&@*            &&&.           ,&&/@.&.@@@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@.%&.&&&.              &&&.             .&&@.&(.@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@.&&&&*&. &             .*,             & .&*&&&&.@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@.&.(&&&     /                        /      @&.&(&.@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@.&&&.&&.         &                   &         .&..&&&.@@@@@@@@@@@@
+  @@@@@@@@@@@@.&*&.&&.            *              /             .&&&.(&.@@@@@@@@@@@
+  @@@@@@@@@@&.&&&&&/                 &         &                 %&/&&&.@@@@@@@@@@
+  @@@@@@@@@.@&&.%&.                    %    .                     .&/&@&&.@@@@@@@@
+  @@@@@@@@.&.&&.&.                   %    &                        .&&&(&&.@@@@@@@
+  @@@@@@/.&&&(&.    @@@@@@@@&&@@@&@&&@&.  &     &@@@@@&@             *&.#&&.(@@@@@
+  @@@@@.@&%%&&.     .@&@@@@&&@@@@@@@@@@@@@@@@@@#  @@@@@@@@&&&&&,      .&*&&&@.@@@@
+  @@@@.&&&&&&              &@@@@@@@@@@@@@@@%#@@@@@&@@@@@@@@@@@@@@@(     &&&&&&.@@@
+  @@.*&&&&&.                 &%&@@@@&@@@@@@@@@@@@@@@@@@@@@@@@@&          .&&&&&,.@
+  @.@&&&&&&.                    &%#   &@@@@@@@@@@@@@@@&@@&@@@@(@@        .&&&&&&&.
+  @@@.&&&&&&&/                            ##@@@@@@@@@( %//#  &        .@&&&&&&*.@@
+  @@@@@..&&&&&&&.                         & ,@@@@@@@@&@@&           .&&&&&&&.#@@@@
+  @@@@@@@@.@&&&&&&.                   .@@@@@@@.    @@@@@          .&&&&&&&.@@@@@@@
+  @@@@@@@@@@.&&&&&&&,               .&&&&&&&&&&&.               %&&&&&&#.@@@@@@@@@
+  @@@@@@@@@@@@..&&&&&&&.          .&&&&&&%%%&&&&&&(          .&&&&&&&.,@@@@@@@@@@@
+  @@@@@@@@@@@@@@@.@&&&&&&&&&&&&&&&&&&&&.%@@@@.&&&&&&&&&&&&&&&&&&&&@.@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@.&&&&&&&&&&&&&&&&&.*@@@@@@@@.&&&&&&&&&&&&&&&&&.@@@@@@@@@@@@@@@@
+  @@@@@@@@@@@@@@@@@@@................@@@@@@@@@@@@...............@@@@@@@@@@@@@@@@@@
+  <![endif]-->
+  </footer></body>
+  </html>
