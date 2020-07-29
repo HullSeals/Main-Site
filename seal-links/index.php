d7472ec14cb80018e23e83bed5ee18d02f66b2e8
Rixxan <rixxan@hullseals.space>
2020-07-29 01:33:35 -0400

    Redo Seal Links

--- seal-links/index.php
+++ seal-links/index.php
@@ -1,71 +1,30 @@
-<?php require_once '../users/init.php';  //make sure this path is correct!
-if (!securePage($_SERVER['PHP_SELF'])){die();}
-$logged_in = $user->data();
-?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+//UserSpice Required
+require_once '../users/init.php';  //make sure this path is correct!
+if (!securePage($_SERVER['PHP_SELF'])){die();}?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <link href="../favicon.ico" rel="icon" type="image/x-icon">
-    <link href="../favicon.ico" rel="shortcut icon" type="image/x-icon">
-    <meta charset="UTF-8">
-    <meta content="Wolfii Namakura" name="author">
-    <meta content="hull seals, elite dangerous, distant worlds, seal team fix, mechanics, dw2" name="keywords">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0" name="viewport">
-    <meta content="Welcome to the Hull Seals, Elite Dangerous's Premier Hull Repair Specialists!" name="description">
+    <meta content="Your Resources for the Seals" name="description">
     <title>Seal Links | The Hull Seals</title>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-    <script src="https://hullseals.space/assets/javascript/allPages.js" integrity="sha384-PsQdnKGi+BdHoxLI6v+pi6WowfGtnraU6GlDD4Uh5Qw2ZFiDD4eWNTNG9+bHL3kf" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="https://hullseals.space/assets/css/allPages.css" />
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js" integrity="sha384-1CmrxMRARb6aLqgBO7yyAxTOQE2AKb9GfXnEo760AUcUmFx3ibVJJAzGytlQcNXd" crossorigin="anonymous"></script>
-    <style>
-    .links article {
-      background-color: rgba(20, 20, 20, 0.7);
-      padding: 0.5rem 3rem;
-      margin: 2rem 20%;
-    }
-    </style>
+    <?php include '../assets/includes/headerCenter.php'; ?>
+    <script>
+    $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
 </head>
-
 <body>
     <div id="home">
-        <header>
-            <nav class="navbar container navbar-expand-lg navbar-expand-md navbar-dark" role="navigation">
-                <a class="navbar-brand" href="../"><img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo"> Hull Seals</a>
-
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="../">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../knowledge">Knowledge Base</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../journal">Journal Reader</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../contact">Contact</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="https://hullseals.space/users/">Login/Register</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <div class="clearfix"></div>
-        <section class="links">
-            <article>
+      <?php include '../assets/includes/menuCode.php';?>
+        <section class="introduction container">
+	    <article id="intro3">
               <div style="text-align: center;">
                 <h1>
                   Seal Links
@@ -74,68 +33,90 @@ $logged_in = $user->data();
                   <h2>Hello, <?php echo echousername($user->data()->id); ?>!</h2>
                 <p>This section contains several Seal internal links. <br />
                   Please do not share private links with non-seals.</p>
-                <p><strong>Not all buttons are active.</strong> Faded buttons indicate inactive links.</p>
               </div>
-                <h3>User and Profile Links</h3>
+                <hr><br><h3>User and Profile Links</h3>
                 <p>
-                <a href="../users/account.php" class="btn btn-primary btn-lg">User Profile Management</a> <a href="../users/manage_sessions.php" class="btn btn-success btn-lg">Session Manager</a> <a href="../users/logout.php" class="btn btn-danger btn-lg">Logout</a>
-
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://hullseals.space/users/account.php" class="btn btn-info">My Profile</a>
+                    <a href="https://hullseals.space/users/manage_sessions.php" class="btn btn-info">Session Management</a>
+                  </div>
                 </p>
-                <h3>Public Links</h3>
-	 	<h4>Be aware: Journal Mat Count is Wrong.</h4>
-		<p>
-                <a href="../journal" class="btn btn-primary btn-lg">Journal Reader</a> <a href="https://hullseals.space/bugreport" class="btn btn-success btn-lg">Bug Reports</a> <a href="https://hullse.al/issuetrack" class="btn btn-primary btn-lg">Issue Tracker</a> <a href="https://hullse.al/FantasticShortlinksAndWhereTheyLead" class="btn btn-success btn-lg">Shortlink List</a>
-                <br /><br />
-                <a href="#" class="btn btn-secondary btn-lg disabled">IRC</a> <a href="../donate" class="btn btn-success btn-lg">Donate</a> <a href="../knowledge" class="btn btn-primary btn-lg">Knowledge Base</a>
+                <hr><br><h3>Public Links</h3>
+		            <p>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://hullseals.space/bugreport" class="btn btn-info">Bug Reports</a>
+                    <a href="https://hullse.al/issuetrack" class="btn btn-info">Issue Tracker</a>
+                    <a href="https://hullseals.space/knowledge/" class="btn btn-info">Knowledge Base</a>
+                  </div>
+                  <br>
+                  <br>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://hullseals.space/SuperSecretDiscordLink" class="btn btn-info">Social Discord</a>
+                    <a href="https://discordapp.com/invite/0hKG2qb9ODixa7Iz" class="btn btn-info">Fleetcomm Discord</a>
+                  </div>
+                  <br>
+                  <br>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://hullseals.space/merch/" class="btn btn-info">Merch Store</a>
+                    <a href="https://hullseals.space/patch-store/" class="btn btn-info">Patches</a>
+                    <a href="https://hullseals.space/donate/" class="btn btn-info">Donations</a>
+                  </div>
                 </p>
-                <h3>Seal Links</h3>
+                <hr><br><h3>Seal Links</h3>
                 <p>
-                <a href="../paperwork" class="btn btn-success btn-lg">Paperwork</a> <a href="#" class="btn btn-secondary btn-lg disabled">TextBin</a>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://hullseals.space/paperwork/" class="btn btn-info">Paperwork</a>
+                    <a href="https://hullseals.space/cmdr-management/" class="btn btn-info">CMDR Management</a>
+                    <a href="https://hullseals.space/vessel-registry/" class="btn btn-info">Vessel Registry</a>
+                  </div>
+                  <br>
+                  <br>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://hullseals.space/trainings/" class="btn btn-info">Training Portal</a>
+                    <a href="https://hullse.al/FantasticShortlinksAndWhereTheyLead" class="btn btn-info">Shortlink List</a>
+                    <a href="#" class="btn btn-info disabled" data-toggle="tooltip" title="Coming Soon!">Textbin</a>
+                  </div>
                 </p>
                 <?php if(hasPerm([5,6],$user->data()->id)){?>
-                <h3>Dispatch Links</h3>
-                <a href="../dispatch-board" class="btn btn-secondary btn-lg disabled">Dispatch Board</a>
+                <hr><br><h3>Dispatch Links</h3>
+                <p>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="#" class="btn btn-info disabled" data-toggle="tooltip" title="Coming Soon!">Dispatch Board</a>
+                    <a href="#" class="btn btn-info disabled" data-toggle="tooltip" title="Coming Soon!">Case Review</a>
+                  </div>
+                </p>
               <?php }?>
               <?php if(hasPerm([4],$user->data()->id)){?>
-                <h3>Trainer Links</h3>
+                <hr><br><h3>Trainer Links</h3>
                 <p>
-                <a href="../trainerLookup" class="btn btn-primary btn-lg">Trainer Record Lookup</a> <a href="../trainerManagement" class="btn btn-secondary btn-lg disabled">Trainer Management Dashbaord</a>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://hullseals.space/trainings/dashboard/" class="btn btn-info">Training Dashboard</a>
+                    <a href="https://hullseals.space/trainings/dashboard/manage.php" class="btn btn-info">Manage Pups</a>
+                    <a href="https://hullseals.space/trainer-lookup/" class="btn btn-info" data-toggle="tooltip" title="Depreciated. Please use Pup Management.">Old Trainer Lookup</a>
+                  </div>
                 </p>
               <?php }?>
                 <?php if(hasPerm([8,9],$user->data()->id)){?>
-                <h3>CyberSeal Links</h3>
+                <hr><br><h3>CyberSeal Links</h3>
                 <p>
-                <a href="../users/admin.php" class="btn btn-primary btn-lg">User Management</a> <a href="https://dev.hullseals.space" class="btn btn-success btn-lg">Beta Site</a> <a href="https://gitlab.com/hull-seals-cyberseals" class="btn btn-primary btn-lg">GitLab</a> <a href="#" class="btn btn-success btn-lg disabled">Password Manager</a> <a href="#" class="btn btn-secondary btn-lg disabled">Backup Site</a>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="https://gitlab.com/hull-seals-cybersealsh" class="btn btn-info">GitLab</a>
+                    <a href="hullseals.space/users/admin.php" class="btn btn-info">User Management</a>
+                    <a href="#" class="btn btn-info disabled">Beta Site</a>
+                  </div>
+                  <br>
+                  <br>
+                  <div class="btn-group btn-group-lg d-flex mx-auto" role="group" style="max-width:85%;">
+                    <a href="#" class="btn btn-info disabled">Password Manager</a>
+                    <a href="#" class="btn btn-info disabled">Fallback Site</a>
+                  </div>
                 </p>
             <?php }?>
               </div>
             </article>
+            <div class="clearfix"></div>
         </section>
-    </div>
-    <footer class="page-footer font-small">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-9 mt-md-0 mt-3">
-                    <h5 class="text-uppercase">Hull Seals</h5>
-                    <p><em>The Hull Seals</em> were established in January of 3305, and have begun plans to roll out galaxy-wide!</p>
-					<a href="https://fuelrats.com/i-need-fuel" class="btn btn-sm btn-secondary">Need Fuel? Call the Rats!</a>
-                </div>
-                <hr class="clearfix w-100 d-md-none pb-3">
-                <div class="col-md-3 mb-md-0 mb-3">
-                    <h5 class="text-uppercase">Links</h5>
-
-                    <ul class="list-unstyled">
-                        <li><a href="https://twitter.com/HullSeals" target="_blank"><img alt="Twitter" height="20" src="../images/twitter_loss.png" width="20"></a> <a href="https://reddit.com/r/HullSeals" target="_blank"><img alt="Reddit" height="20" src="../images/reddit.png" width="20"></a> <a href="https://www.youtube.com/channel/UCwKysCkGU_C6V8F2inD8wGQ" target="_blank"><img alt="Youtube" height="20" src="../images/youtube.png" width="20"></a> <a href="https://www.twitch.tv/hullseals" target="_blank"><img alt="Twitch" height="20" src="../images/twitch.png" width="20"></a> <a href="https://gitlab.com/hull-seals-cyberseals" target="_blank"><img alt="GitLab" height="20" src="../images/gitlab.png" width="20"></a></li>
-						<li><a href="/donate">Donate</a></li>
-                        <li><a href="https://hullseals.space/knowledge/books/important-information/page/privacy-policy">Privacy & Cookies Policy</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            Site content copyright © 2019, The Hull Seals. All Rights Reserved. Elite Dangerous and all related marks are trademarks of Frontier Developments Inc.
-        </div>
-    </footer>
-</body>
-
-</html>
+      </div>
+      <?php include '../assets/includes/footer.php'; ?>
+  </body>
+  </html>
